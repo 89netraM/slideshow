98b3b50f77a209678464440268b343497055d5f5
Mårten Åsberg <marten.asberg@outlook.com>
2018-07-15 20:48:05 +0200

    The start of a control panel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
 	//tslint:disable-next-line:component-selector
@@ -6,6 +6,9 @@ import { Component } from "@angular/core";
 	templateUrl: "./app.component.html",
 	styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-	title = "app";
+export class AppComponent implements OnInit {
+
+	public ngOnInit(): void {
+
+	}
 }
