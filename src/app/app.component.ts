8f1e75d895cbc17ebe818a40f0519be59969b06d
Mårten Åsberg <marten.asberg@outlook.com>
2018-07-18 20:21:07 +0200

    A nice info box

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from "@angular/core";
 	styleUrls: ["./app.component.scss"]
 })
 export class AppComponent implements OnInit {
+	public showInfo: boolean = false;
 
 	public ngOnInit(): void {
 
